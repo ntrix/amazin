3285f351b5bb8eab94e15005fa9c763d3ec55d0a
ntrix <ntrix@ntien.com>
2020-09-09 17:37:20 +0200

    order route

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,122 @@
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+import Order from "../models/orderModel.js";
+import { isAdmin, isAuth, isSellerOrAdmin } from "../utils.js";
+
+const orderRoute = express.Router();
+
+orderRoute.use(isAuth);
+
+orderRoute.get(
+  "/",
+  isSellerOrAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const seller = req.query.seller || "";
+    const sellerFilter = seller ? { seller } : {};
+
+    const orders = await Order.find({ ...sellerFilter }).populate(
+      "user",
+      "name"
+    );
+    res.send(orders);
+  })
+);
+
+orderRoute.get(
+  "/mine",
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
+  })
+);
+
+orderRoute.post(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    if (req.body.orderItems.length === 0) {
+      res.status(400).send({ message: "Cart is empty" });
+    } else {
+      const order = new Order({
+        seller: req.body.orderItems[0].seller,
+        orderItems: req.body.orderItems,
+        shippingAddress: req.body.shippingAddress,
+        paymentMethod: req.body.paymentMethod,
+        itemsPrice: req.body.itemsPrice,
+        shippingPrice: req.body.shippingPrice,
+        taxPrice: req.body.taxPrice,
+        totalPrice: req.body.totalPrice,
+        user: req.user._id,
+      });
+      const createdOrder = await order.save();
+      res
+        .status(201)
+        .send({ message: "New Order Created", order: createdOrder });
+    }
+  })
+);
+
+orderRoute.get(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({ message: "Order Not Found" });
+    }
+  })
+);
+
+orderRoute.put(
+  "/:id/pay",
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      };
+      const updatedOrder = await order.save();
+      res.send({ message: "Order Paid", order: updatedOrder });
+    } else {
+      res.status(404).send({ message: "Order Not Found" });
+    }
+  })
+);
+
+orderRoute.delete(
+  "/:id",
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      const deleteOrder = await order.remove();
+      res.send({ message: "Order Deleted", order: deleteOrder });
+    } else {
+      res.status(404).send({ message: "Order Not Found" });
+    }
+  })
+);
+
+orderRoute.put(
+  "/:id/deliver",
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
+
+      const updatedOrder = await order.save();
+      res.send({ message: "Order Delivered", order: updatedOrder });
+    } else {
+      res.status(404).send({ message: "Order Not Found" });
+    }
+  })
+);
+
+export default orderRoute;
