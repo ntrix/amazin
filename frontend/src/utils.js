563c5eaa1b785bed376137aaf25f0c157d4ce6ab
ntrix <ntrix@ntien.com>
2021-02-26 18:49:38 +0100

    add more price filter

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,6 +1,6 @@
 export const prices = [
   {
-    name: 'Any',
+    name: "Any",
     min: 0,
     max: 0,
   },
@@ -19,25 +19,30 @@ export const prices = [
     min: 100,
     max: 1000,
   },
+  {
+    name: `$1000 to $10000`,
+    min: 1000,
+    max: 10000,
+  },
 ];
 export const ratings = [
   {
-    name: '4stars & up',
+    name: "4stars & up",
     rating: 4,
   },
 
   {
-    name: '3stars & up',
+    name: "3stars & up",
     rating: 3,
   },
 
   {
-    name: '2stars & up',
+    name: "2stars & up",
     rating: 2,
   },
 
   {
-    name: '1stars & up',
+    name: "1stars & up",
     rating: 1,
   },
 ];
