8aa659fdc7c40651026a80e9f1681f8606cda532
ntrix <ntrix@ntien.com>
2021-03-06 02:07:56 +0100

    refac:extract mainRoute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { BrowserRouter, Link, Route } from "react-router-dom";
-
-import { signout } from "./Controllers/userActions";
-import { listProductCategories } from "./Controllers/productActions";
-
-import SigninScreen from "./Features/Auth/SigninScreen";
-import RegisterScreen from "./Features/Auth/RegisterScreen";
-
-import AdminRoute from "./Features/Route/AdminRoute";
-import PrivateRoute from "./Features/Route/PrivateRoute";
-import SellerRoute from "./Features/Route/SellerRoute";
-
-import CartScreen from "./Features/Checkout/CartScreen";
-import PaymentMethodScreen from "./Features/Checkout/PaymentMethodScreen";
-import ShippingAddressScreen from "./Features/Checkout/ShippingAddressScreen";
-
-import OrderHistoryScreen from "./Features/Order/OrderHistoryScreen";
-import OrderListScreen from "./Features/Order/OrderListScreen";
-import OrderScreen from "./Features/Order/OrderScreen";
-import PlaceOrderScreen from "./Features/Order/PlaceOrderScreen";
-
-import ProductEditScreen from "./Features/Product/ProductEditScreen";
-import ProductListScreen from "./Features/Product/ProductListScreen";
-import ProductScreen from "./Features/Product/ProductScreen";
-import SearchScreen from "./Features/Product/SearchScreen";
-
-import MapScreen from "./Features/User/MapScreen";
-import ProfileScreen from "./Features/User/ProfileScreen";
-import UserEditScreen from "./Features/User/UserEditScreen";
-import UserListScreen from "./Features/User/UserListScreen";
-
-import HomeScreen from "./Features/HomeScreen";
-import SellerScreen from "./Features/SellerScreen";
-
-import SearchBox from "./components/SearchBox";
+import { BrowserRouter, Link } from "react-router-dom";
 import LoadingBox from "./components/LoadingBox";
 import MessageBox from "./components/MessageBox";
+import SearchBox from "./components/SearchBox";
+import { listProductCategories } from "./Controllers/productActions";
+import { signout } from "./Controllers/userActions";
+import MainRoute from "./Features/Route/MainRoute";
 import Logo from "./img/a.svg";
-import Cart from "./img/cart.png";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -322,85 +291,7 @@ function App() {
           htmlFor="btn--close-sidebar"
         ></label>
         <main className="container--flex">
-          <Route path="/seller/:id" component={SellerScreen}></Route>
-          <Route path="/cart/:id?" component={CartScreen}></Route>
-          <Route path="/product/:id" component={ProductScreen} exact></Route>
-          <Route
-            path="/product/:id/edit"
-            component={ProductEditScreen}
-            exact
-          ></Route>
-          <Route path="/signin" component={SigninScreen}></Route>
-          <Route path="/register" component={RegisterScreen}></Route>
-          <Route path="/shipping" component={ShippingAddressScreen}></Route>
-          <Route path="/payment" component={PaymentMethodScreen}></Route>
-          <Route path="/place-order" component={PlaceOrderScreen}></Route>
-          <Route path="/order/:id" component={OrderScreen}></Route>
-          <Route
-            path="/search/name/:name?"
-            component={SearchScreen}
-            exact
-          ></Route>
-          <Route
-            path="/search/category/:category"
-            component={SearchScreen}
-            exact
-          ></Route>
-          <Route
-            path="/search/category/:category/name/:name"
-            component={SearchScreen}
-            exact
-          ></Route>
-          <Route
-            path="/search/category/:category/name/:name/min/:min/max/:max/rating/:rating/order/:order/pageNumber/:pageNumber"
-            component={SearchScreen}
-            exact
-          ></Route>
-          <PrivateRoute
-            path="/order-history"
-            component={OrderHistoryScreen}
-          ></PrivateRoute>
-          <PrivateRoute
-            path="/profile"
-            component={ProfileScreen}
-            exact
-          ></PrivateRoute>
-          <PrivateRoute
-            path="/profile/seller"
-            component={ProfileScreen}
-            exact
-          ></PrivateRoute>
-          <PrivateRoute path="/map" component={MapScreen}></PrivateRoute>
-          <AdminRoute
-            path="/product-list"
-            component={ProductListScreen}
-            exact
-          ></AdminRoute>
-          <AdminRoute
-            path="/product-list/pageNumber/:pageNumber"
-            component={ProductListScreen}
-            exact
-          ></AdminRoute>
-          <AdminRoute
-            path="/order-list"
-            component={OrderListScreen}
-            exact
-          ></AdminRoute>
-          <AdminRoute path="/user-list" component={UserListScreen}></AdminRoute>
-          <AdminRoute
-            path="/user/:id/edit"
-            component={UserEditScreen}
-          ></AdminRoute>
-          <SellerRoute
-            path="/product-list/seller"
-            component={ProductListScreen}
-          ></SellerRoute>
-          <SellerRoute
-            path="/order-list/seller"
-            component={OrderListScreen}
-          ></SellerRoute>
-
-          <Route path="/" component={HomeScreen} exact></Route>
+          <MainRoute />
         </main>
         <footer className="row center">
           Amazin' eCommerce platform, all right reserved
---
+++ frontend/src/Features/Route/MainRoute.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+import RegisterScreen from "../Auth/RegisterScreen";
+import SigninScreen from "../Auth/SigninScreen";
+import CartScreen from "../Checkout/CartScreen";
+import PaymentMethodScreen from "../Checkout/PaymentMethodScreen";
+import ShippingAddressScreen from "../Checkout/ShippingAddressScreen";
+import HomeScreen from "../HomeScreen";
+import OrderHistoryScreen from "../Order/OrderHistoryScreen";
+import OrderListScreen from "../Order/OrderListScreen";
+import OrderScreen from "../Order/OrderScreen";
+import PlaceOrderScreen from "../Order/PlaceOrderScreen";
+import ProductEditScreen from "../Product/ProductEditScreen";
+import ProductListScreen from "../Product/ProductListScreen";
+import ProductScreen from "../Product/ProductScreen";
+import SearchScreen from "../Product/SearchScreen";
+import AdminRoute from "./AdminRoute";
+import PrivateRoute from "./PrivateRoute";
+import SellerRoute from "./SellerRoute";
+import SellerScreen from "../SellerScreen";
+import MapScreen from "../User/MapScreen";
+import ProfileScreen from "../User/ProfileScreen";
+import UserEditScreen from "../User/UserEditScreen";
+import UserListScreen from "../User/UserListScreen";
+
+export default function MainRoute() {
+  return (
+    <>
+      <Route path="/seller/:id" component={SellerScreen}></Route>
+      <Route path="/cart/:id?" component={CartScreen}></Route>
+      <Route path="/product/:id" component={ProductScreen} exact></Route>
+      <Route
+        path="/product/:id/edit"
+        component={ProductEditScreen}
+        exact
+      ></Route>
+      <Route path="/signin" component={SigninScreen}></Route>
+      <Route path="/register" component={RegisterScreen}></Route>
+      <Route path="/shipping" component={ShippingAddressScreen}></Route>
+      <Route path="/payment" component={PaymentMethodScreen}></Route>
+      <Route path="/place-order" component={PlaceOrderScreen}></Route>
+      <Route path="/order/:id" component={OrderScreen}></Route>
+      <Route path="/search/name/:name?" component={SearchScreen} exact></Route>
+      <Route
+        path="/search/category/:category"
+        component={SearchScreen}
+        exact
+      ></Route>
+      <Route
+        path="/search/category/:category/name/:name"
+        component={SearchScreen}
+        exact
+      ></Route>
+      <Route
+        path="/search/category/:category/name/:name/min/:min/max/:max/rating/:rating/order/:order/pageNumber/:pageNumber"
+        component={SearchScreen}
+        exact
+      ></Route>
+      <PrivateRoute
+        path="/order-history"
+        component={OrderHistoryScreen}
+      ></PrivateRoute>
+      <PrivateRoute
+        path="/profile"
+        component={ProfileScreen}
+        exact
+      ></PrivateRoute>
+      <PrivateRoute
+        path="/profile/seller"
+        component={ProfileScreen}
+        exact
+      ></PrivateRoute>
+      <PrivateRoute path="/map" component={MapScreen}></PrivateRoute>
+      <AdminRoute
+        path="/product-list"
+        component={ProductListScreen}
+        exact
+      ></AdminRoute>
+      <AdminRoute
+        path="/product-list/pageNumber/:pageNumber"
+        component={ProductListScreen}
+        exact
+      ></AdminRoute>
+      <AdminRoute
+        path="/order-list"
+        component={OrderListScreen}
+        exact
+      ></AdminRoute>
+      <AdminRoute path="/user-list" component={UserListScreen}></AdminRoute>
+      <AdminRoute path="/user/:id/edit" component={UserEditScreen}></AdminRoute>
+      <SellerRoute
+        path="/product-list/seller"
+        component={ProductListScreen}
+      ></SellerRoute>
+      <SellerRoute
+        path="/order-list/seller"
+        component={OrderListScreen}
+      ></SellerRoute>
+
+      <Route path="/" component={HomeScreen} exact></Route>
+    </>
+  );
+}
