cae510d439d073700bb41bd7e6bebe21fc5db272
ntrix <ntrix@ntien.com>
2021-03-23 12:23:02 +0100

    !!fixes proxy,multer,img upload,cloudinary folder

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -1,22 +1,80 @@
 import multer from "multer";
 import express from "express";
 import { isAuth } from "../utils.js";
+import cloudinary from "cloudinary";
 
 const uploadRoute = express.Router();
 
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename(req, file, cb) {
-    cb(null, `${Date.now()}.jpg`);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination(req, file, cb) {
+//     cb(null, "/images/uploads/");
+//   },
+//   filename(req, file, cb) {
+//     cb(null, `${Date.now()}.jpg`);
+//   },
+// });
 
-const upload = multer({ storage });
+// const upload = multer({ storage });
+
+// uploadRoute.post(
+//   "/",
+//   isAuth,
+//   upload.single("image"),
+//   (req, res, (err) => res.status(504).send({ message: err })) => {
+//     res.send(`/${req.file.path}`);
+//   }
+// );
+const upload = multer({ dest: "-tmp/uploads/" });
 
 uploadRoute.post("/", isAuth, upload.single("image"), (req, res) => {
-  res.send(`/${req.file.path}`);
+  cloudinary.config({
+    cloud_name: process.env.CD_NAME,
+    api_key: process.env.CD_API_KEY,
+    api_secret: process.env.CD_API_SECRET,
+  });
+
+  const image = req.file;
+  console.log({ image });
+  (async () => {
+    if (image && image.path)
+      await cloudinary.v2.uploader.upload(
+        image.path,
+        { folder: "amazin" },
+        (error, data) => {
+          if (error) return res.status(401).send({ message: error });
+          res.send(data.url);
+        }
+      );
+    else res.status(404).send({ message: "No Image has been sent" });
+  })();
 });
 
 export default uploadRoute;
+
+/*
+router.put('/:id/profile',
+    userController.putUserUpdate,
+);
+
+exports.putUserUpdate = function (req, res, next) {
+    const { first_name, last_name, cover_photo, avatar, bio } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.errors);
+    }
+
+    (async () => {
+        const updatedUser = {
+            first_name,
+            last_name,
+            bio,
+        };
+        if (cover_photo) await cloudinary.uploader.upload(cover_photo, {}, (err, data) => updatedUser.cover_photo = data.url);
+        if (avatar) await cloudinary.uploader.upload(avatar, {}, (err, data) => updatedUser.avatar = data.url);
+        User.findByIdAndUpdate(req.params.id, updatedUser, { new: true })
+        .then( _ => res.status(200).json({ message: 'Your profile has been updated' }) )
+        .catch( err => { res.status(200).json({ message: 'no profile update' }); next(err)} );
+    }) ();
+};
+
+*/
--- frontend/src/Features/Product/ProductEditScreen.js
+++ frontend/src/Features/Product/ProductEditScreen.js
@@ -23,6 +23,7 @@ export default function ProductEditScreen(props) {
   const [countInStock, setCountInStock] = useState("");
   const [brand, setBrand] = useState("");
   const [description, setDescription] = useState("");
+  const [imagePreview, setImagePreview] = useState("");
 
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
@@ -79,6 +80,35 @@ export default function ProductEditScreen(props) {
 
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
+
+  const uploadFileHandler1 = (e, setPic, setPreview) => {
+    const file = e.target.files[0];
+    setImagePreview(URL.createObjectURL(e.target.files[0]));
+    const reader = new FileReader();
+    setLoadingUpload(true);
+    reader.readAsDataURL(file);
+    reader.onload = async () => {
+      const { result } = reader;
+      try {
+        const { data } = await Axios.post(
+          "/api/uploads",
+          { image: result },
+          {
+            headers: {
+              enctype: "multipart/form-data",
+              Authorization: `Bearer ${userInfo.token}`,
+            },
+          }
+        );
+        setImage(data);
+        setLoadingUpload(false);
+      } catch (error) {
+        setErrorUpload(error.message);
+        setLoadingUpload(false);
+      }
+    };
+  };
+
   const uploadFileHandler = async (e) => {
     const file = e.target.files[0];
     const bodyFormData = new FormData();
@@ -87,7 +117,7 @@ export default function ProductEditScreen(props) {
     try {
       const { data } = await Axios.post("/api/uploads", bodyFormData, {
         headers: {
-          "Content-Type": "multipart/form-data",
+          enctype: "multipart/form-data",
           Authorization: `Bearer ${userInfo.token}`,
         },
       });
@@ -171,6 +201,11 @@ export default function ProductEditScreen(props) {
                 label="Choose Image"
                 onChange={uploadFileHandler}
               ></input>
+
+              <div>
+                <img src={imagePreview || image} alt="" className="medium" />
+              </div>
+
               {loadingUpload && <LoadingBox />}
               {errorUpload && (
                 <MessageBox variant="danger">{errorUpload}</MessageBox>
