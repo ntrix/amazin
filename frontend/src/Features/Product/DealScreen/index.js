61829cd947cc02942c2bb811e5c80dfee07069a9
ntrix <ntrix@ntien.com>
2021-03-24 18:32:46 +0100

    pagination:z-index over divider screen-header:cat-nav:overflow,hover padding,more place for tablet loadingBox:stable placeholder on each screen

--- frontend/src/Features/Product/DealScreen/index.js
+++ frontend/src/Features/Product/DealScreen/index.js
@@ -60,37 +60,35 @@ export default function DealScreen() {
       <div
         className={"deal-screen" + (Math.random() < 0.5 ? "" : " screen--1")}
       >
-        {loading ? (
-          <LoadingBox xl />
-        ) : error ? (
-          <MessageBox variant="danger">{error}</MessageBox>
-        ) : (
-          <Carousel
-            swipeable={true}
-            draggable={true}
-            showDots={true}
-            responsive={responsive}
-            infinite={true}
-            autoPlay={true}
-            autoPlaySpeed={3000}
-            keyBoardControl={true}
-            customTransition="transform 500ms ease-in-out"
-            transitionDuration={500}
-            centerMode={true}
-            containerClass="carousel-container"
-            removeArrowOnDeviceType={["mobile"]}
-            dotListClass="custom-dot-list-style"
-            itemClass="carousel-item-padding-40-px"
-          >
-            {products.length === 0 ? (
-              <MessageBox>No Deals On This Category!</MessageBox>
-            ) : (
-              products.map((product, id) => (
-                <ProductCard deal key={id} product={product}></ProductCard>
-              ))
-            )}
-          </Carousel>
-        )}
+        <Carousel
+          swipeable={true}
+          draggable={true}
+          showDots={true}
+          responsive={responsive}
+          infinite={true}
+          autoPlay={true}
+          autoPlaySpeed={3000}
+          keyBoardControl={true}
+          customTransition="transform 500ms ease-in-out"
+          transitionDuration={500}
+          centerMode={true}
+          containerClass="carousel-container"
+          removeArrowOnDeviceType={["mobile"]}
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+        >
+          {loading ? (
+            <LoadingBox xl />
+          ) : error ? (
+            <MessageBox variant="danger">{error}</MessageBox>
+          ) : products.length === 0 ? (
+            <MessageBox>No Deals On This Category!</MessageBox>
+          ) : (
+            products.map((product, id) => (
+              <ProductCard deal key={id} product={product}></ProductCard>
+            ))
+          )}
+        </Carousel>
         <h2 className="mh-2">Top Deals</h2>
         <div className="row top">
           <div className="row search__banner">
