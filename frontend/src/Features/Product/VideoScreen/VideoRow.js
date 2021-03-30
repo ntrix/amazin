98a709ef702d619fe89d0bc752211d3965c2f860
ntrix <ntrix@ntien.com>
2021-03-30 23:02:04 +0200

    fixes:videoScreen unmounted component videoRow

--- frontend/src/Features/Product/VideoScreen/VideoRow.js
+++ frontend/src/Features/Product/VideoScreen/VideoRow.js
@@ -1,41 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Carousel, { dummyMovies, responsive } from "../../../utils";
 import UTube from "./UTube";
 import VideoCard from "./VideoCard";
 
 export default function VideoRow({ title, movies = [], portrait = false }) {
   const [trailerUrl, setTrailerUrl] = useState("");
+  let isMounted = false;
+
+  useEffect(() => {
+    isMounted = true;
+    return () => (isMounted = false);
+  }, []);
 
   return (
     <div className="m-row">
       <h2>{title}</h2>
-      <Carousel
-        swipeable={true}
-        draggable={true}
-        showDots={false}
-        responsive={responsive}
-        infinite={false}
-        autoPlay={false}
-        keyBoardControl={true}
-        customTransition="transform 300ms ease-in-out"
-        transitionDuration={500}
-        containerClass="carousel-container"
-        removeArrowOnDeviceType={["mobile"]}
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
-      >
-        {(movies || dummyMovies).map((movie, id) => (
-          <VideoCard
-            key={id}
-            movie={movie}
-            portrait={portrait}
-            trailerUrl={trailerUrl}
-            setTrailerUrl={setTrailerUrl}
-          />
-        ))}
-      </Carousel>
+      {isMounted && (
+        <>
+          <Carousel
+            swipeable={true}
+            draggable={true}
+            showDots={false}
+            responsive={responsive}
+            infinite={false}
+            autoPlay={false}
+            keyBoardControl={true}
+            customTransition="transform 300ms ease-in-out"
+            transitionDuration={500}
+            containerClass="carousel-container"
+            removeArrowOnDeviceType={["mobile"]}
+            dotListClass="custom-dot-list-style"
+            itemClass="carousel-item-padding-40-px"
+          >
+            {(movies || dummyMovies).map((movie, id) => (
+              <VideoCard
+                key={id}
+                movie={movie}
+                portrait={portrait}
+                trailerUrl={trailerUrl}
+                setTrailerUrl={setTrailerUrl}
+              />
+            ))}
+          </Carousel>
 
-      <UTube trailerUrl={trailerUrl} />
+          <UTube trailerUrl={trailerUrl} />
+        </>
+      )}
     </div>
   );
 }
