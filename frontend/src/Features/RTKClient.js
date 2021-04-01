74aaa0ebe63e456fbf364ccb47cd3c6917dd25f7
ntrix <ntrix@ntien.com>
2021-04-01 19:40:27 +0200

    fixes:HTML error response by disconnecting

--- frontend/src/Features/RTKClient.js
+++ frontend/src/Features/RTKClient.js
@@ -3,7 +3,13 @@ export { createSlice } from "@reduxjs/toolkit";
 export const Reducer = (stateKeyName) => ({
   _REQUEST: (state, action) => ({ loading: true }),
   _SUCCESS: (state, action) =>
-    stateKeyName === "..." //noname saved state => destructuring
+    typeof action.payload === "string" && action.payload.startsWith("<!") // Error HTML response?
+      ? {
+          loading: false,
+          error: "Couldn't access Database Server!",
+          success: false,
+        }
+      : stateKeyName === "..." //noname saved state => destructuring
       ? {
           ...action.payload,
           loading: false,
