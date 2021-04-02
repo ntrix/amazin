f57bab1e716e078aa7bc1ac6b924726d502598a4
ntrix <ntrix@ntien.com>
2021-04-02 03:28:51 +0200

    BE baseURL to env

--- frontend/src/Controllers/axiosClient.js
+++ frontend/src/Controllers/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "https://amazin-be.herokuapp.com",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   mode: "cors",
 });
 
