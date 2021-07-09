b84a6df3f23acddb66729e17aa88ad7a6f57776c
ntrix <ntrix@ntien.com>
2021-07-09 16:51:23 +0200

    refactor: RTKClient

--- frontend/src/Features/RTKClient.js
+++ frontend/src/Features/RTKClient.js
@@ -2,24 +2,26 @@ export { createSlice } from "@reduxjs/toolkit";
 
 export const Reducer = (stateKeyName) => ({
   _REQUEST: (state, action) => ({ loading: true }),
-  _SUCCESS: (state, action) =>
-    typeof action.payload === "string" && action.payload.startsWith("<!") // Error HTML response?
+  _SUCCESS: (state, action) => {
+    if (typeof action.payload === "string" && action.payload.startsWith("<!"))
+      // Error HTML response?
+      return {
+        loading: false,
+        error: "Couldn't access Database Server!",
+        success: false,
+      };
+    return stateKeyName === "..." // noname saved state? Array? => destructuring
       ? {
-          loading: false,
-          error: "Couldn't access Database Server!",
-          success: false,
-        }
-      : stateKeyName === "..." //noname saved state => destructuring
-      ? {
-          ...action.payload,
+          ...action.payload, // e.g. { a:1, b:2, c:3, loading, success }
           loading: false,
           success: true,
         }
       : {
-          [stateKeyName]: action.payload,
+          [stateKeyName]: action.payload, // e.g. { state[a]:1, loading, success }
           loading: false,
           success: true,
-        },
+        };
+  },
   _FAIL: (state, action) => ({
     loading: false,
     error: action.payload,
