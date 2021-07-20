738dffad35e23f834dedf1cef46b6e79893d01f8
ntrix <ntrix@ntien.com>
2021-07-20 11:06:04 +0200

    error pages

--- apps/amazin/src/Features/Auth/ErrorFallBack.js
+++ apps/amazin/src/Features/Auth/ErrorFallBack.js
@@ -1,14 +1,30 @@
+import { Link } from 'react-router-dom';
 import MessageBox from '../../components/MessageBox';
-import Screen404 from './Screen404';
 
 /* TODO send errors report to server using Websocket */
 const ErrorFallback = ({ error }) => (
   <div className="container">
     <div className="home-screen">
-      <div className="home__banner home error-page">
-        <MessageBox variant="danger" msg={error.message} />
+      <div className="c-screen customer">
+        <h1 className="container">SORRY! Something wrong happens!</h1>
+        <div className="divider-inner"></div>
+
+        <div className="container">
+          <MessageBox variant="danger" msg={error.message} />
+        </div>
+        <div className="divider-inner"></div>
+
+        <div className="container">
+          <Link to="/customer">
+            <button className="primary mb-1">Report this Error</button>
+          </Link>
+
+          <Link to="/">
+            <button className="ml-1 mb-1">Back to Homepage</button>
+          </Link>
+        </div>
       </div>
-      <Screen404 />
+      <div className="divider"></div>
     </div>
   </div>
 );
