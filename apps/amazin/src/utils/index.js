201daddccfb329fb2644efc3a5dc8b2393dd89df
ntrix <ntrix@ntien.com>
2021-07-29 00:23:20 +0200

    extracts findSuggest from utils

--- apps/amazin/src/utils/index.js
+++ apps/amazin/src/utils/index.js
@@ -1,4 +1,5 @@
 import { CURR_FORMAT, SRC_URL, NO_IMAGE, KEY } from '../constants';
+export { findSuggest } from './findSuggest';
 
 /* debounce & throttle for useShadow */
 export function debounce(func, duration) {
@@ -118,67 +119,6 @@ export const sourceAdapter = (movies) =>
 
 /* create an array of 12 dummyMovies (a row) for videoRow(s) */
 export const dummyMovies = sourceAdapter(Array(12).fill(1));
-
-/* find suggestions util. for searchBox's dropdown suggest list */
-export const findSuggest = (() => {
-  const openTag = '<b>';
-  const closeTag = '</b>';
-  // eslint-disable-next-line
-  const escapeC = (s) => s.replace(/[\-#$\^*()+\[\]{}|\\,\'\"\&.?\s]/g, '\\$&');
-
-  const combinePhrases = new RegExp(escapeC(closeTag + openTag), 'g');
-
-  const group = new RegExp(
-    `(${escapeC(openTag)}[\\s\\S]+?${escapeC(closeTag)})`,
-    'g'
-  );
-
-  const findPriority = (string, word) => {
-    let prior = 0;
-    word = openTag + word + closeTag;
-    string.replace(group, (found) => {
-      prior = word === found ? 999 : Math.max(found.length, prior);
-    });
-    return prior;
-  };
-
-  return {
-    search(list, keyword) {
-      if (!list || !keyword) return [];
-
-      keyword = keyword.slice(0, 49);
-      const splittedKeys = keyword.split('');
-
-      const convertedKey = splittedKeys.reduce(
-        (acc, char) => `${acc}(${escapeC(char)})(.*?)`,
-        '(.*?)'
-      );
-      const regKey = new RegExp(convertedKey, 'i');
-
-      const replacer = splittedKeys.reduce(
-        (acc, _, i) => `${acc}${openTag}$${i * 2 + 2}${closeTag}$${i * 2 + 3}`,
-        '$1'
-      );
-
-      const result = list.reduce(
-        (acc, item) =>
-          regKey.test(item.name)
-            ? acc.concat({
-                name: item.name
-                  .replace(regKey, replacer)
-                  .replace(combinePhrases, ''),
-                _id: item._id
-              })
-            : acc,
-        []
-      );
-
-      return result.sort(
-        (a, b) => findPriority(b.name, keyword) - findPriority(a.name, keyword)
-      );
-    }
-  };
-})();
 
 export function shortName(user, length) {
   if (!user) return 'Sign In';
