8026b813318f11a255ff2eaa66e9a79d5e62d2d7
ntrix <ntrix@ntien.com>
2021-08-19 14:04:42 +0200

    story CustomInput

---
+++ apps/amazin/src/components/stories/CustomInput.stories.js
@@ -0,0 +1,34 @@
+import CustomInput from '../CustomInput';
+
+export default {
+  title: 'Components/Global/CustomInput',
+  component: CustomInput
+};
+
+const Template = (args) => <CustomInput {...args} />;
+const text = 'Sample Text Input';
+const Args = {
+  text,
+  type: 'text',
+  placeholder: `Enter ${text}`,
+  hook: {},
+  textarea: false,
+  label: text,
+  wrapClass: 'flex-col',
+  rest: null
+};
+
+export const SameLabel = Template.bind(Args);
+SameLabel.args = { ...Args };
+
+const OptionalLabelArgs = { ...Args, label: 'Set Or Remove Label' };
+export const OptionalLabel = Template.bind(OptionalLabelArgs);
+OptionalLabel.args = { ...OptionalLabelArgs };
+
+const InlineArgs = { ...Args, wrapClass: '' };
+export const Inline = Template.bind(InlineArgs);
+Inline.args = { ...InlineArgs };
+
+const TextAreaArgs = { ...Args, textarea: true };
+export const TextArea = Template.bind(TextAreaArgs);
+TextArea.args = { ...TextAreaArgs };
