a139a3b2097b25432c16e8b48f8c86133d31d93c
ntrix <ntrix@ntien.com>
2021-08-25 11:50:53 +0200

    naming

--- apps/amazin/src/__tests__/App.js
+++ apps/amazin/src/__tests__/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
-import { AppProviders } from '../mock';
+import { AppProviders } from '../__mocks__';
 import userEvent from '@testing-library/user-event';
 import('@testing-library/jest-dom/extend-expect');
 
