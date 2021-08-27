be0b46ebcde22f6d73cd51d09477d19591c4e95c
ntrix <ntrix@NTien.com>
2021-08-27 16:15:41 +0200

    moves Suspense layout

--- apps/amazin/src/routes/AdminRoute.js
+++ apps/amazin/src/routes/AdminRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Redirect, Route } from './RouteBoundary';
+import { Redirect, Route } from '../layouts/SuspenseRoute';
 
 export default function AdminRoute({ component: Component, ...rest }) {
   const { userInfo } = useSelector((state) => state.userSignin);
