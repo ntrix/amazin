a089dd9a3658608d6ae9b31e5b0bf3aee8f07767
ntrix <ntrix@NTien.com>
2021-08-27 14:44:02 +0200

    moves store

--- apps/amazin/.storybook/preview.js
+++ apps/amazin/.storybook/preview.js
@@ -1,8 +1,8 @@
 import '../src/index.css';
-import '../src/Features/Nav/nav.css';
+import '../src/components/Nav/nav.css';
 import '../src/Features/Product/VideoScreen/videoScreen.css';
 import '../src/Features/User/CustomerScreen/customerScreen.css';
-import '../src/responsive.css';
+import '../src/app/responsive.css';
 import '../src/fonts/fonts.css';
 import '../src/fonts/font-awesome.css';
 
