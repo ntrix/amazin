7d43615230448d7734ab0da87e51874a66cf46a6
ntrix <ntrix@NTien.com>
2021-08-28 23:40:20 +0200

    minor fixes warning

--- apps/amazin/src/components/Nav/NavBelt/NavUser/useMenuCreator.js
+++ apps/amazin/src/components/Nav/NavBelt/NavUser/useMenuCreator.js
@@ -1,4 +1,4 @@
-import { lazy, useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { signout } from '../../../../apis/userAPI';
 import { signinTemplate } from './signinTemplate';
@@ -6,19 +6,18 @@ import { createUserTemplate } from './userTemplate';
 import { createSellerTemplate } from './sellerTemplate';
 import { adminTemplate } from './adminTemplate';
 import { shortName } from '../../../../utils';
-const DropdownUser = lazy(() => import(/* webpackPrefetch: true */ './DropdownUser'));
 
 export default function useMenuCreator() {
   const dispatch = useDispatch();
-  const signOutCb = useCallback(() => dispatch(signout()), [dispatch]);
+  const signoutHandler = useCallback(() => dispatch(signout()), [dispatch]);
   const { userInfo } = useSelector((state) => state.userSignin);
   const [userName, setUserName] = useState('');
   const [userMenu, setUserMenu] = useState([]);
   const [sellerMenu, setSellerMenu] = useState([]);
   useEffect(() => {
     setUserName(!userInfo ? 'Sign in' : shortName(userInfo.name, 7));
-    setUserMenu(!userInfo ? signinTemplate : createUserTemplate(userInfo, signOutCb));
+    setUserMenu(!userInfo ? signinTemplate : createUserTemplate(userInfo, signoutHandler));
     setSellerMenu(createSellerTemplate(userInfo));
-  }, [userInfo]);
+  }, [userInfo, signoutHandler]);
   return { userInfo, userName, userMenu, sellerMenu, adminMenu: adminTemplate };
 }
