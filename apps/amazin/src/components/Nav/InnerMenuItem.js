a1e21f977b7a403c44248f571506b425d1cdc4f0
ntrix <ntrix@NTien.com>
2021-08-29 02:10:26 +0200

    refact: MenuItem

---
+++ apps/amazin/src/components/Nav/InnerMenuItem.js
@@ -0,0 +1,25 @@
+import { memo } from 'react';
+import { useHistory } from 'react-router';
+
+function InnerMenuItem({ label, to, className, clearShadow, extFunction, children }) {
+  const history = useHistory();
+  if (!to) return className ? <div>{label}</div> : <strong>{label}</strong>;
+  if (to === 'disabled') return <div className="menu__link-item disabled" disabled children={label} />;
+  if (to.startsWith('https://')) return <a href={to} target="_blank" rel="noreferrer" children={label} />;
+  return (
+    <div
+      className={'menu__link-item ' + className}
+      aria-label={label || className}
+      onClick={(e) => {
+        e.stopPropagation();
+        clearShadow('');
+        if (extFunction) extFunction();
+        history.push(to);
+      }}
+    >
+      {children}
+      {label}
+    </div>
+  );
+}
+export default memo(InnerMenuItem);
