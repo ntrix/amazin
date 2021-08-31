7f943470e038523a767f35ecb3bed3597b67c67d
ntrix <ntrix@NTien.com>
2021-08-31 22:38:45 +0200

    lines

--- apps/amazin/src/screens/User/ContactScreen/index.js
+++ apps/amazin/src/screens/User/ContactScreen/index.js
@@ -20,22 +20,20 @@ export default function ContactScreen() {
   const { submitContact } = useSubmitContact(setLoading, setMessage, setError);
 
   return (
-    <div>
-      <form className="form" onSubmit={(e) => submitContact(e, { name, email, subject, text })}>
-        <h1>Contact Us</h1>
-        <LoadingOrError xl statusOf={{ loading, error }} />
+    <form className="form" onSubmit={(e) => submitContact(e, { name, email, subject, text })}>
+      <h1>Contact Us</h1>
+      <LoadingOrError xl statusOf={{ loading, error }} />
 
-        <SuccessModal msg={message} label="Back To Home Page" />
-        {!message && (
-          <>
-            <CustomInput text="Your Name" hook={[name, setName]} />
-            <CustomInput text="Email" type="email" hook={[email, setEmail]} />
-            <ContactSubject hook={[subject, setSubject]} />
-            <CustomInput textarea rows="10" text="Your Message" hook={[text, setText]} />
-            <Button primary type="submit" className="col-fill mt-2" label="Send Your Message" />
-          </>
-        )}
-      </form>
-    </div>
+      <SuccessModal msg={message} label="Back To Home Page" />
+      {!message && (
+        <>
+          <CustomInput text="Your Name" hook={[name, setName]} />
+          <CustomInput text="Email" type="email" hook={[email, setEmail]} />
+          <ContactSubject hook={[subject, setSubject]} />
+          <CustomInput textarea rows="10" text="Your Message" hook={[text, setText]} />
+          <Button primary type="submit" className="col-fill mt-2" label="Send Your Message" />
+        </>
+      )}
+    </form>
   );
 }
