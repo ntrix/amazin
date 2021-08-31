0eef0aed075219311db425efc03b4d5f4e364ae2
ntrix <ntrix@NTien.com>
2021-08-31 03:22:37 +0200

    refact: RegisterScreen

---
+++ apps/amazin/src/screens/Auth/RegisterScreen/index.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { useRegisterEffect } from './useRegisterEffect';
+import Button from 'src/components/Button';
+import CustomInput from 'src/components/CustomInput';
+import LoadingOrError from 'src/components/LoadingOrError';
+
+export default function RegisterScreen({ location, history }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const { redirect, userRegister, submitRegister } = useRegisterEffect(location, history);
+
+  return (
+    <div>
+      <form className="form" onSubmit={(e) => submitRegister(e, { name, email, password, confirmPassword })}>
+        <h1>Create Account</h1>
+        <LoadingOrError statusOf={userRegister} />
+
+        <CustomInput text="Name" required hook={[name, setName]} />
+        <CustomInput text="Email" type="email" required hook={[email, setEmail]} />
+        <CustomInput text="Password" type="password" required hook={[password, setPassword]} />
+        <CustomInput text="Confirm Password" type="password" required hook={[confirmPassword, setConfirmPassword]} />
+        <br />
+        <Button primary className="mt-1 col-fill" type="submit" label="Register" />
+
+        <div>
+          <div className="mt-1">
+            Already have an account? <Link to={`/signin?redirect=${redirect}`} children={<b>Sign-In</b>} />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
