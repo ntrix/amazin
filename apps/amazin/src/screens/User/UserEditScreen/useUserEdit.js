5d000aa0a9c8607ebc0dad7b1f15f2880a910042
ntrix <ntrix@NTien.com>
2021-09-02 10:23:52 +0200

    refact: UserEditScreen: cleans up, extracts hooks to useUserEdit

---
+++ apps/amazin/src/screens/User/UserEditScreen/useUserEdit.js
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { detailsUser, updateUser } from 'src/apis/userAPI';
+import { userUpdateActions } from 'src/slice/UserSlice';
+
+export function useUserEdit(setName, setEmail, setSeller, setAdmin, history, match) {
+  const dispatch = useDispatch();
+  const paramUserId = match.params.id;
+  const { user, loading, error } = useSelector((state) => state.userDetails);
+  const userUpdate = useSelector((state) => state.userUpdate);
+
+  useEffect(() => {
+    if (userUpdate.success) {
+      dispatch(userUpdateActions._RESET());
+      history.push('/user-list');
+    }
+
+    if (!user) dispatch(detailsUser(paramUserId));
+    else {
+      setName(user.name);
+      setEmail(user.email);
+      setSeller(user.isSeller);
+      setAdmin(user.isAdmin);
+    } // eslint-disable-next-line
+  }, [user, userUpdate.success, paramUserId, history, dispatch]);
+
+  const submitUser = (name, email, seller, admin) => (e) => {
+    e.preventDefault();
+    dispatch(updateUser({ name, email, isSeller: seller, isAdmin: admin, _id: paramUserId }));
+  };
+
+  return { user, loading, error, userUpdate, submitUser };
+}
