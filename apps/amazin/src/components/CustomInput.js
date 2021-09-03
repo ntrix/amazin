672f9307a3aafbda79070a36e9669c198e8d84e9
ntrix <ntrix@NTien.com>
2021-09-03 11:28:34 +0200

    cleans up

--- apps/amazin/src/components/CustomInput.js
+++ apps/amazin/src/components/CustomInput.js
@@ -4,27 +4,21 @@ function CustomInput({
   text,
   type = 'text',
   placeholder = `Enter ${text}`,
-  hook,
+  hook: [state, setState] = [],
   textarea = false,
   label = text,
   wrapClass = '',
   ...rest
 }) {
-  const id = text.split(' ').join('-').toLowerCase();
-  const props = Object.assign(rest, { id, text, type, placeholder });
-
-  if (hook) {
-    props.value = hook[0];
-    props.onChange = (e) => hook[1](e.target.value);
-  }
-
-  const { value } = rest;
-  const val = type === 'button' || type === 'submit' ? text : value;
+  const id = text.split(' ').join('-').toLowerCase(); // create #id for .css
+  const onChange = setState ? (e) => setState(e.target.value) : undefined;
+  const value = type === 'button' || type === 'submit' ? text : state || rest.value;
+  const props = { id, text, type, placeholder, ...rest, value, onChange };
 
   return (
     <div className={wrapClass}>
-      {label !== 'none' && <label htmlFor={props.id}>{label + ' '}</label>}
-      {textarea ? <textarea {...props} /> : <input value={val} {...props} />}
+      {label !== 'none' && <label htmlFor={id}>{label + ' '}</label>}
+      {textarea ? <textarea {...props} /> : <input {...props} />}
     </div>
   );
 }
