bf70af5e93fc3006bc00709f48acc8faceaeea97
ntrix <ntrix@NTien.com>
2021-09-04 00:21:50 +0200

    LoadingOrError reduces complex, creates InnerBox comp

--- apps/amazin/src/components/LoadingOrError.js
+++ apps/amazin/src/components/LoadingOrError.js
@@ -4,25 +4,28 @@ import { SHOW_ERROR_TIMEOUT } from '../constants';
 import LoadingBox from './LoadingBox';
 import MessageBox from './MessageBox';
 
-function LoadingOrError({ statusOf, wrapClass = '', errorMsg = '', xl = false, variant, children, ...rest }) {
+function InnerBox({ statusOf, errorMsg = '', xl = false, variant, children, ...rest }) {
   const { loading = false, error = false } = statusOf;
+
+  if (loading) return <LoadingBox xl={xl} />;
+  if (error) return <MessageBox msg={errorMsg || error} variant={variant ?? 'danger'} />;
+  return children ? cloneElement(children, Object.assign(children.props, rest)) : null;
+}
+
+function LoadingOrError({ wrapClass = '', ...props }) {
   const [hideError, setHideError] = useState(false);
   const timeoutId = useRef(null);
 
   useEffect(() => {
-    if (error) {
+    if (props.statusOf?.error) {
       setHideError(false);
       timeoutId.current = setTimeout(() => setHideError(true), SHOW_ERROR_TIMEOUT);
     }
     return () => clearTimeout(timeoutId.current);
-  }, [error]);
+  }, [props.statusOf.error]);
 
-  const innerComponent = () => {
-    if (loading) return <LoadingBox xl={xl} />;
-    if (error && !hideError) return <MessageBox msg={errorMsg || error} variant={variant ?? 'danger'} />;
-    return children ? cloneElement(children, Object.assign(children.props, rest)) : null;
-  };
-
-  return !wrapClass || !children ? innerComponent() : <div className={wrapClass}>{innerComponent()}</div>;
+  if (hideError) return null;
+  if (!wrapClass || !props.children) return <InnerBox {...props} />;
+  return <div className={wrapClass} children={<InnerBox {...props} />} />;
 }
 export default memo(LoadingOrError);
