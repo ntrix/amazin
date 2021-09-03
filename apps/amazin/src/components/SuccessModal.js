11fd3225d8ef7b6ceff1b4f9989462167b14b9f3
ntrix <ntrix@NTien.com>
2021-09-04 00:03:45 +0200

    minor

--- apps/amazin/src/components/SuccessModal.js
+++ apps/amazin/src/components/SuccessModal.js
@@ -3,14 +3,12 @@ import MessageBox from 'src/components/MessageBox';
 
 export default function SuccessModal({ msg, back = '/', label = 'Back To Your Last Session' }) {
   if (!msg) return null;
-
+  /* TODO shadow under modal?? */
   return (
     <>
-      <MessageBox variant="success" show>
-        {msg}
-      </MessageBox>
+      <MessageBox variant="success" msg={msg} />
       <br />
-      <Button to={back} primary label={label} />
+      <Button primary to={back} label={label} />
       <div className="separator divider-inner" />
     </>
   );
