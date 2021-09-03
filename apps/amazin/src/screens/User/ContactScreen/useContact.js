39312ab1d99e321166d14398504aa4b9b594da57
ntrix <ntrix@NTien.com>
2021-09-03 21:26:41 +0200

    reduces complex. useSubmitContact

--- apps/amazin/src/screens/User/ContactScreen/useContact.js
+++ apps/amazin/src/screens/User/ContactScreen/useContact.js
@@ -1,8 +1,53 @@
 import { useEffect } from 'react';
-import { useParams } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { useParams } from 'react-router';
+import { axios } from 'src/apis/axiosClient';
+import { updateUserProfile } from 'src/apis/userAPI';
 import { userUpdateProfileActions } from 'src/slice/UserSlice';
+
+const MAIL_SERVER = process.env.REACT_APP_CONTACT_MAIL_SERVER;
+const HEADERS = { mode: 'cors', headers: { 'Content-Type': 'application/json' } };
+
+const checkError = ({ text, email, name }) => {
+  const errors = [];
+  const validate = (err, msg) => (err ? errors.push(msg) : null);
+
+  validate(!text, 'Please enter your message!');
+  validate(!email, 'Please enter your email!');
+  validate(!name, 'Please enter your name!');
+  return errors;
+};
+
+export function useSubmitContact(setLoading, setMessage, setError) {
+  const dispatch = useDispatch();
+  const { subject: paramSub } = useParams();
+  const { userInfo } = useSelector((state) => state.userSignin);
+
+  const submitContact = async (e, data = {}) => {
+    e.preventDefault();
+
+    const errors = checkError(data);
+    if (errors.length) return setError(errors);
+
+    if ('Seller' === data.subject) return dispatch(updateUserProfile({ userId: userInfo._id, verify: true }));
+
+    dispatch(userUpdateProfileActions._RESET());
+    setMessage('Thank you! Your message has been sent.');
+    setLoading(true); // "/api/user/contact"
+    try {
+      await axios.post(MAIL_SERVER, data, { headers: { ...HEADERS, body: JSON.stringify(data) } });
+      setLoading(false);
+
+      if ('Admin' === paramSub) return setMessage('Please wait 48 hours for processing!');
+    } catch (err) {
+      setLoading(false);
+      setError([err.message]);
+    }
+    return null;
+  };
+
+  return { submitContact };
+}
 
 export function useContact(setName, setEmail, setSubject, setMessage) {
   const dispatch = useDispatch();
