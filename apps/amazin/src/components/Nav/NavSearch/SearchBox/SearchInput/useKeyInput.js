f6714d461cd4f33f697c92ed4ed3d4786dfed4bf
ntrix <ntrix@NTien.com>
2021-09-04 00:47:59 +0200

    reduces complex

--- apps/amazin/src/components/Nav/NavSearch/SearchBox/SearchInput/useKeyInput.js
+++ apps/amazin/src/components/Nav/NavSearch/SearchBox/SearchInput/useKeyInput.js
@@ -11,23 +11,27 @@ export function useKeyInput(setInput, setSuggests, submitSearch) {
   const { shadowOf, setShadowOf } = useShadow();
 
   const handleKeyInput = ({ target: { value }, key }) => {
-    if (value.length === 0 || key === 'Escape') {
-      setSuggestBox(false);
-      return setShadowOf('');
+    switch (key) {
+      case 'Enter':
+        return submitSearch();
+      case 'Escape':
+        if (value.length === 0) {
+          setSuggestBox(false);
+          return setShadowOf('');
+        } else return null;
+      default: {
+        setSuggestBox(true);
+        const newSuggests = findSuggest(productList, value);
+
+        if (SHADOW.NAV_SEARCH !== shadowOf && newSuggests.length) {
+          setShadowOf(SHADOW.NAV_SEARCH);
+          setOutline(true);
+        }
+        setSuggests(newSuggests);
+        setInput(value);
+        return null;
+      }
     }
-
-    if (key === 'Enter') return submitSearch();
-
-    setSuggestBox(true);
-    const newSuggests = findSuggest(productList, value);
-
-    if (SHADOW.NAV_SEARCH !== shadowOf && newSuggests.length) {
-      setShadowOf(SHADOW.NAV_SEARCH);
-      setOutline(true);
-    }
-    setSuggests(newSuggests);
-    setInput(value);
-    return null;
   };
 
   return { handleKeyInput };
