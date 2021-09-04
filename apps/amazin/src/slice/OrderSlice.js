3c8974775e20f61a101fe59a3515bb3a9f2a7ea0
ntrix <ntrix@NTien.com>
2021-09-04 05:11:46 +0200

    refact slices, add adapter utils

--- apps/amazin/src/slice/OrderSlice.js
+++ apps/amazin/src/slice/OrderSlice.js
@@ -1,43 +1,27 @@
-import { createSlice, Reducer } from './ReduxToolKitClient';
+import { adapter, createSlice, Reducer } from './ReduxToolKitClient';
 
-export const { actions: orderCreateActions, reducer: orderCreateReducer } = createSlice({
-  name: 'orderCreate',
-  initialState: {},
-  reducers: Reducer('order')
-});
+export const { actions: orderCreateActions, reducer: orderCreateReducer } = createSlice(
+  adapter('orderCreate', {}, Reducer('order'))
+);
 
-export const { actions: orderDetailsActions, reducer: orderDetailsReducer } = createSlice({
-  name: 'orderDetails',
-  initialState: { loading: true },
-  reducers: Reducer('order')
-});
+export const { actions: orderDetailsActions, reducer: orderDetailsReducer } = createSlice(
+  adapter('orderDetails', { loading: true }, Reducer('order'))
+);
 
-export const { actions: orderPayActions, reducer: orderPayReducer } = createSlice({
-  name: 'orderPay',
-  initialState: {},
-  reducers: Reducer()
-});
+export const { actions: orderPayActions, reducer: orderPayReducer } = createSlice(adapter('orderPay', {}, Reducer()));
 
-export const { actions: orderMineListActions, reducer: orderMineListReducer } = createSlice({
-  name: 'orderMineList',
-  initialState: { orders: [] },
-  reducers: Reducer('orders')
-});
+export const { actions: orderMineListActions, reducer: orderMineListReducer } = createSlice(
+  adapter('orderMineList', { orders: [] }, Reducer('orders'))
+);
 
-export const { actions: orderListActions, reducer: orderListReducer } = createSlice({
-  name: 'orderList',
-  initialState: { orders: [] },
-  reducers: Reducer('orders')
-});
+export const { actions: orderListActions, reducer: orderListReducer } = createSlice(
+  adapter('orderList', { orders: [] }, Reducer('orders'))
+);
 
-export const { actions: orderDeleteActions, reducer: orderDeleteReducer } = createSlice({
-  name: 'orderDelete',
-  initialState: {},
-  reducers: Reducer()
-});
+export const { actions: orderDeleteActions, reducer: orderDeleteReducer } = createSlice(
+  adapter('orderDelete', {}, Reducer())
+);
 
-export const { actions: orderDeliverActions, reducer: orderDeliverReducer } = createSlice({
-  name: 'orderDeliver',
-  initialState: {},
-  reducers: Reducer()
-});
+export const { actions: orderDeliverActions, reducer: orderDeliverReducer } = createSlice(
+  adapter('orderDeliver', {}, Reducer())
+);
--- apps/amazin/src/slice/UserSlice.js
+++ apps/amazin/src/slice/UserSlice.js
@@ -1,60 +1,38 @@
-import { createSlice, Reducer } from './ReduxToolKitClient';
+import { adapter, createSlice, Reducer } from './ReduxToolKitClient';
 import { KEY } from '../constants';
 
-export const { actions: userRegisterActions, reducer: userRegisterReducer } = createSlice({
-  name: 'userRegister',
-  initialState: {},
-  reducers: Reducer(KEY.USER_INFO)
-});
+export const { actions: userRegisterActions, reducer: userRegisterReducer } = createSlice(
+  adapter('userRegister', {}, Reducer(KEY.USER_INFO))
+);
 
-export const { actions: userSigninActions, reducer: userSigninReducer } = createSlice({
-  name: 'userSignin',
-  initialState: {},
-  reducers: Reducer(KEY.USER_INFO)
-});
+export const { actions: userSigninActions, reducer: userSigninReducer } = createSlice(
+  adapter('userSignin', {}, Reducer(KEY.USER_INFO))
+);
 
-export const { actions: userDetailsActions, reducer: userDetailsReducer } = createSlice({
-  name: 'userDetails',
-  initialState: { loading: true },
-  reducers: { ...Reducer('user'), _RESET: () => ({ loading: true }) }
-});
+export const { actions: userDetailsActions, reducer: userDetailsReducer } = createSlice(
+  adapter('userDetails', { loading: true }, { ...Reducer('user'), _RESET: () => ({ loading: true }) })
+);
 
-export const { actions: userUpdateProfileActions, reducer: userUpdateProfileReducer } = createSlice({
-  name: 'userUpdateProfile',
-  initialState: {},
-  reducers: Reducer()
-});
+export const { actions: userUpdateProfileActions, reducer: userUpdateProfileReducer } = createSlice(
+  adapter('userUpdateProfile', {}, Reducer())
+);
 
-export const { actions: userUpdateActions, reducer: userUpdateReducer } = createSlice({
-  name: 'userUpdate',
-  initialState: {},
-  reducers: Reducer()
-});
+export const { actions: userUpdateActions, reducer: userUpdateReducer } = createSlice(
+  adapter('userUpdate', {}, Reducer())
+);
 
-export const { actions: userListActions, reducer: userListReducer } = createSlice({
-  name: 'userList',
-  initialState: { loading: true },
-  reducers: Reducer('users')
-});
+export const { actions: userListActions, reducer: userListReducer } = createSlice(
+  adapter('userList', { loading: true }, Reducer('users'))
+);
 
-export const { actions: userDeleteActions, reducer: userDeleteReducer } = createSlice({
-  name: 'userDelete',
-  initialState: {},
-  reducers: Reducer()
-});
+export const { actions: userDeleteActions, reducer: userDeleteReducer } = createSlice(
+  adapter('userDelete', {}, Reducer())
+);
 
-export const { actions: userTopSellerListActions, reducer: userTopSellerListReducer } = createSlice({
-  name: 'userTopSellerList',
-  initialState: { loading: true },
-  reducers: Reducer('users')
-});
+export const { actions: userTopSellerListActions, reducer: userTopSellerListReducer } = createSlice(
+  adapter('userTopSellerList', { loading: true }, Reducer('users'))
+);
 
-export const { actions: userAddressMapActions, reducer: userAddressMapReducer } = createSlice({
-  name: 'userAddressMap',
-  initialState: {},
-  reducers: {
-    _CONFIRM: (state, action) => ({
-      address: action.payload
-    })
-  }
-});
+export const { actions: userAddressMapActions, reducer: userAddressMapReducer } = createSlice(
+  adapter('userAddressMap', {}, { _CONFIRM: (state, action) => ({ address: action.payload }) })
+);
