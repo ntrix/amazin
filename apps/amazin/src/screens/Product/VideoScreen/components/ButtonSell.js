47fa7f681360b06d0fb7124e768cb96acb720963
ntrix <ntrix@NTien.com>
2021-09-05 15:26:26 +0200

    removes duplicates, ProductList & ButtonSell use same hooks

--- apps/amazin/src/screens/Product/VideoScreen/components/ButtonSell.js
+++ apps/amazin/src/screens/Product/VideoScreen/components/ButtonSell.js
@@ -1,9 +1,7 @@
-import { memo, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { memo } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { createProduct } from 'src/apis/productAPI';
-import { productCreateActions } from 'src/slice/ProductSlice';
+import { useProductList } from '../../ProductListScreen/useProductList';
 
 export const SellerButton = (props) => (
   <button className="banner__button mh-2" {...props}>
@@ -12,23 +10,10 @@ export const SellerButton = (props) => (
 );
 
 function ButtonSell() {
-  const dispatch = useDispatch();
   const history = useHistory();
-  const { userInfo } = useSelector((state) => state.userSignin);
-  const productCreate = useSelector((state) => state.productCreate);
+  const { userInfo, createHandler } = useProductList(history);
 
-  useEffect(() => {
-    if (productCreate.success) {
-      dispatch(productCreateActions._RESET());
-      history.push(`/product/${productCreate.product._id}/edit`);
-    }
-  }, [productCreate, dispatch, history]);
-
-  const createSellingProduct = () => {
-    dispatch(createProduct());
-  };
-
-  return <SellerButton disabled={!userInfo?.isSeller} onClick={createSellingProduct} />;
+  return <SellerButton disabled={!userInfo?.isSeller} onClick={createHandler} />;
 }
 
 export default memo(ButtonSell);
