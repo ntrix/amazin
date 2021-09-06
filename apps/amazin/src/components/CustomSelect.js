cd53577cfe9690979dad3164e5a7844a9382e6db
ntrix <ntrix@NTien.com>
2021-09-06 20:04:39 +0200

    fixes CustomSelect quantity Number removed func, & e to e.target.value

--- apps/amazin/src/components/CustomSelect.js
+++ apps/amazin/src/components/CustomSelect.js
@@ -1,12 +1,17 @@
 import { MAX_ITEM } from 'src/constants';
 import { createId } from 'src/utils';
 
+const createList = (max) => {
+  const _max = Math.min(MAX_ITEM, max);
+  const list = Array(_max).fill(0);
+  return list.map((_, key) => ({ value: key + 1 }));
+};
+
 const Selects = ({ list }) =>
   list.map(({ value, children }) => <option key={value} value={value} children={children || value} />);
 
-export default function CustomSelect({ label, wrapClass = '', optgroup = '', list, max = 1, small = false, ...props }) {
-  const _max = Math.min(MAX_ITEM, max);
-  const selects = list || [...Array(_max).keys()].map((key) => ({ value: key + 1 }));
+export default function CustomSelect({ label, wrapClass = '', optgroup = '', list, max, small = false, ...props }) {
+  const selectList = list || createList(max || 1);
 
   return (
     <>
@@ -14,7 +19,13 @@ export default function CustomSelect({ label, wrapClass = '', optgroup = '', lis
       <div className={'select-wrapper ' + wrapClass}>
         <div className={`sprite__caret${small ? '' : ' xl'}`} />
         <select id={createId(optgroup || label)} className="tab__w6" {...props}>
-          {optgroup ? <optgroup label={optgroup} children={<Selects list={selects} />} /> : <Selects list={selects} />}
+          {optgroup ? (
+            <optgroup label={optgroup}>
+              <Selects list={selectList} />
+            </optgroup>
+          ) : (
+            <Selects list={selectList} />
+          )}
         </select>
       </div>
     </>
