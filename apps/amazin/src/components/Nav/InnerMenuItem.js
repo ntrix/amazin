c6583f537ac4a9ecc50741e2d22ef1f8fb18c0ad
ntrix <ntrix@NTien.com>
2021-09-06 20:33:35 +0200

    try: reduces complex.

--- apps/amazin/src/components/Nav/InnerMenuItem.js
+++ apps/amazin/src/components/Nav/InnerMenuItem.js
@@ -1,30 +1,36 @@
 import { memo } from 'react';
 import { useHistory } from 'react-router';
 
-function InnerMenuItem({ label, to, className, clearShadow, extFunction, children }) {
+function InnerMenuItem({ label, to = '', className, clearShadow, extFunction, children }) {
   const history = useHistory();
 
-  if (!to) return className ? <div>{label}</div> : <strong>{label}</strong>;
+  switch (to.slice(0, 8)) {
+    case '':
+      return className ? <div>{label}</div> : <strong>{label}</strong>;
 
-  if (to === 'disabled') return <div className="menu__link-item disabled" disabled children={label} />;
+    case 'disabled':
+      return <div className="menu__link-item disabled" disabled children={label} />;
 
-  if (to.startsWith('https://')) return <a href={to} target="_blank" rel="noreferrer" children={label} />;
+    case 'https://':
+      return <a href={to} target="_blank" rel="noreferrer" children={label} />;
 
-  return (
-    <div
-      className={'menu__link-item ' + className}
-      aria-label={`${label} ${className}`}
-      onClick={(e) => {
-        e.stopPropagation();
-        clearShadow('');
-        if (extFunction) extFunction();
-        history.push(to);
-      }}
-    >
-      {children}
-      {label}
-    </div>
-  );
+    default:
+      return (
+        <div
+          className={'menu__link-item ' + className}
+          aria-label={`${label} ${className}`}
+          onClick={(e) => {
+            e.stopPropagation();
+            clearShadow('');
+            if (extFunction) extFunction();
+            history.push(to);
+          }}
+        >
+          {children}
+          {label}
+        </div>
+      );
+  }
 }
 
 export default memo(InnerMenuItem);
