0b31dcf04052e9b196db455ae0af0c7797846df0
ntrix <ntrix@NTien.com>
2021-09-08 14:02:37 +0200

    try: VideoScreen Suspense, Fallbacks, skeleton!!!, refact useMovieList, cleans up & extracts async fn

--- apps/amazin/src/screens/Product/VideoScreen/useMovieList.js
+++ apps/amazin/src/screens/Product/VideoScreen/useMovieList.js
@@ -1,59 +1,53 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 
 import './videoScreen.css';
 import { listProducts } from 'src/apis/productAPI';
-import { IN_STOCK, VIDEO } from 'src/constants';
+import { STORE, IN_STOCK, VIDEO } from 'src/constants';
 import { useSafeState } from 'src/hooks/useSafeState';
-import { sourceAdapter } from 'src/utils';
+import { dummyMovies, sourceAdapter } from 'src/utils';
 
-export function useExternMovies() {
-  const [externMovies, setExternMovies] = useSafeState({});
+function fetchMovies() {
+  const movieGenres = Object.keys(VIDEO.SRC);
 
-  useEffect(() => {
-    (async function fetchData() {
-      const _movieList = {};
-      const movieGenres = Object.keys(VIDEO.SRC);
-
-      await Promise.all(
-        movieGenres.map(async (genre) => {
-          const { data } = await axios.get(VIDEO.URL + VIDEO.SRC[genre]).catch();
-          _movieList[genre] = sourceAdapter(data.results);
-        })
-      );
-      setExternMovies(_movieList);
-    })();
-  }, [setExternMovies]);
-
-  return { externMovies };
+  return Promise.all(
+    movieGenres.map(async (genre) => {
+      const { data } = await axios.get(VIDEO.URL + VIDEO.SRC[genre]).catch();
+      return [genre, sourceAdapter(data.results)];
+    })
+  ).then(Object.fromEntries);
 }
 
 export function useMovieList() {
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
-  const { products, success } = productList;
+  const { products } = productList;
   const productCreate = useSelector((state) => state.productCreate);
-  const [stockMovies, setStockMovies] = useSafeState({ [IN_STOCK]: [] });
-  const [bannerMovies, setBannerMovies] = useSafeState([]);
-  const { externMovies } = useExternMovies();
+  const [bannerMovies, setBannerMovies, isMounted] = useSafeState({});
+  const [stockMovies, setStockMovies] = useState({ [IN_STOCK]: dummyMovies });
+  const [externMovies, setExternMovies] = useState({ [STORE]: dummyMovies });
 
   useEffect(() => {
+    fetchMovies().then((movies) => (isMounted.current ? setExternMovies(movies) : null));
     dispatch(listProducts({ seller: VIDEO.SELLER, category: 'Video', pageSize: 11, order: 'oldest' }));
-  }, [dispatch]);
+  }, [isMounted, dispatch]);
 
   useEffect(() => {
-    const _banner = {};
-    VIDEO.GENRES.forEach((_genre) => {
-      const genreMovies = !success ? VIDEO.EMPTY : externMovies[_genre] || stockMovies[IN_STOCK] || VIDEO.EXAMPLES;
-      _banner[_genre] = genreMovies[(Math.random() * genreMovies.length) | 0];
+    if (!isMounted.current || stockMovies[IN_STOCK] !== dummyMovies) return;
+
+    if (products?.[0].seller?._id === VIDEO.SELLER) setStockMovies({ [IN_STOCK]: products });
+  }, [isMounted, products, stockMovies]);
+
+  useEffect(() => {
+    if (externMovies[STORE] === dummyMovies || stockMovies[IN_STOCK] === dummyMovies) return;
+
+    const bannerMoviesArray = VIDEO.GENRES.map((genre) => {
+      const genreMovies = externMovies[genre] || stockMovies[IN_STOCK];
+      return [genre, genreMovies[(Math.random() * genreMovies.length) | 0]];
     });
-    setBannerMovies(_banner);
-  }, [success, setBannerMovies, externMovies, stockMovies]);
+    setBannerMovies(Object.fromEntries(bannerMoviesArray));
+  }, [externMovies, stockMovies, setBannerMovies]);
 
-  useEffect(() => {
-    if (products) setStockMovies({ [IN_STOCK]: products.filter((p) => p.seller._id === VIDEO.SELLER) });
-  }, [products, setStockMovies]);
-
-  return { stockList: productList, productCreate, externMovies, stockMovies, bannerMovies };
+  return { productList, productCreate, externMovies, stockMovies, bannerMovies };
 }
