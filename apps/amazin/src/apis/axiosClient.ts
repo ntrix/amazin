a2a35ceaed79ff1f79e036bd18b0d61f14b3ef18
ntrix <ntrix@NTien.com>
2021-09-11 23:55:22 +0200

    ts: suspenseAP I!! refact axiosClient reduces complex.

--- apps/amazin/src/apis/axiosClient.ts
+++ apps/amazin/src/apis/axiosClient.ts
@@ -5,38 +5,45 @@ const axiosClient = axios.create({
   headers: { mode: 'cors' }
 });
 
-const getTokenHeader = (getState: AppState) => ({
-  Authorization: `Bearer ${getState()?.userSignin?.userInfo?.token}`,
-  mode: 'cors'
-});
+const getTokenHeader = (authorization: boolean, getState: AppState) =>
+  !authorization
+    ? undefined
+    : {
+        Authorization: `Bearer ${getState()?.userSignin?.userInfo?.token}`,
+        mode: 'cors'
+      };
+
+const checkDBError = (data: unknown) => {
+  // HTML response with Error?
+  if (typeof data === 'string' && data.startsWith('<!')) throw new Error("Couldn't access Database Server!");
+};
 
 const axiosRedux =
-  (authorization: true | undefined) =>
+  (authorization: boolean) =>
   (
     [action, actionBySuccess = action, actionByFail = actionBySuccess]: SliceAction[],
     { successAction, successHandler, selector = (d) => d }: OptionFns = {}
   ) =>
   (method: Method | undefined = 'get', url = '', requestData?: unknown) =>
   async (dispatch: AppDispatch, getState: AppState) => {
-    const headers = authorization && getTokenHeader(getState);
+    const headers = getTokenHeader(authorization, getState);
     url = process.env.REACT_APP_BACKEND_URL + url;
 
     dispatch(action._REQUEST());
 
     try {
       const { data } = await axios(url, { method, headers, data: requestData });
-      // HTML response with Error?
-      if (typeof data === 'string' && data.startsWith('<!')) throw new Error("Couldn't access Database Server!");
 
+      checkDBError(data);
       dispatch(actionBySuccess._SUCCESS(selector(data)));
       if (successAction) dispatch(successAction(data));
       if (successHandler) successHandler(data);
     } catch (error) {
-      if (error instanceof Error) dispatch(actionByFail._FAIL(error.message));
+      if (error instanceof Error) dispatch(actionByFail._FAIL(error?.message));
     }
   };
 
-const axiosPublic = axiosRedux(undefined);
+const axiosPublic = axiosRedux(false);
 const axiosPrivate = axiosRedux(true);
 
 export default axiosClient;
