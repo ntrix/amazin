81fc069da5b1aa756b00e13a09d5eac8168698a0
ntrix <ntrix@NTien.com>
2021-09-14 21:27:56 +0200

    extract constants

---
+++ apps/amazin/src/constants/storageRedux.ts
@@ -0,0 +1,8 @@
+/* localStorage and Redux */
+export const KEY = {
+  USER_INFO: 'userInfo',
+  CART_ITEMS: 'cartItems',
+  SHIPPING_ADDRESS: 'shippingAddress',
+  CURRENCY: 'currency',
+  HISTORY: 'history'
+};
