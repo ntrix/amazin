315ac7c5258f7944b1a25f59ededffd5df9aeb07
ntrix <ntrix@NTien.com>
2021-09-14 14:34:44 +0200

    ts screen ProductList, useShadow, Pagination

--- apps/amazin/src/screens/Product/ProductListScreen/Table.tsx
+++ apps/amazin/src/screens/Product/ProductListScreen/Table.tsx
@@ -2,11 +2,19 @@ import Button from 'src/components/Button';
 import CheckCell from 'src/components/CheckCell';
 import BaseTable from 'src/layouts/BaseTable';
 
-export default function Table({ header, keys, data, deleteHandler, to }) {
+type PropType = {
+  header: string[];
+  keys: string[];
+  products: ProductType[];
+  deleteHandler?: FnType;
+  to?: string;
+};
+
+export default function Table({ header, keys, products, deleteHandler, to }: PropType) {
   return (
     <BaseTable
       header={header.map((h) => h.toUpperCase())}
-      body={data.map((row) => (
+      body={products.map((row) => (
         <tr key={row._id}>
           {keys.map((col, id) => (
             <CheckCell key={`${row._id} ${id}`} children={row[col]} />
