4db028ac5fdb11164f4c8521f32d6e1e677c69bb
ntrix <ntrix@NTien.com>
2021-09-17 13:10:16 +0200

    ts CartSlice createReducers type

--- apps/amazin/src/slice/ReduxToolKitClient.ts
+++ apps/amazin/src/slice/ReduxToolKitClient.ts
@@ -1,31 +1,33 @@
 import { SliceCaseReducers } from '@reduxjs/toolkit';
 
 export { createSlice } from '@reduxjs/toolkit';
-export const createReducers = (stateKeyName = '', overwriteReducers?: SliceCaseReducers<AppState>) => ({
-  _REQUEST: () => ({ loading: true }),
-  _SUCCESS: (state: AppState, action: SliceAction) => {
-    // noname saved state?
-    return stateKeyName === '...'
-      ? {
-          ...action.payload, // Array? => destructuring e.g. { a:1, b:2, c:3, loading, success }
-          loading: false,
-          success: true
-        }
-      : {
-          [stateKeyName]: action.payload, // e.g. { state[a]:1, loading, success }
-          loading: false,
-          success: true
-        };
-  },
-  _FAIL: (state: AppState, action: SliceAction) => ({
-    loading: false,
-    error: action.payload
-  }),
-  _RESET: () => ({}),
-  ...overwriteReducers
-});
 
-export const adapter = (name: string, initialState: AppState, reducers: SliceCaseReducers<AppState>) => ({
+export const createReducers: FnType = (stateKeyName = '', overwriteReducers?: SliceCaseReducers<unknown>) =>
+  ({
+    _REQUEST: () => ({ loading: true }),
+    _SUCCESS: (state: AppState, action: SliceAction) => {
+      // noname saved state?
+      return stateKeyName === '...'
+        ? {
+            ...action.payload, // Array? => destructuring e.g. { a:1, b:2, c:3, loading, success }
+            loading: false,
+            success: true
+          }
+        : {
+            [stateKeyName]: action.payload, // e.g. { state[a]:1, loading, success }
+            loading: false,
+            success: true
+          };
+    },
+    _FAIL: (state: AppState, action: SliceAction) => ({
+      loading: false,
+      error: action.payload
+    }),
+    _RESET: () => ({}),
+    ...overwriteReducers
+  } as SliceCaseReducers<unknown>);
+
+export const adapter: FnType = (name: string, initialState: unknown, reducers: SliceCaseReducers<unknown>) => ({
   initialState,
   reducers,
   name
