f4dd2219521cf3cff1498d891cf62ee7729eb4da
ntrix <ntrix@NTien.com>
2021-09-19 16:37:16 +0200

    refact MainRoutes extract SearchRoutes reduce fn lines

---
+++ apps/amazin/src/routes/SearchRoutes.tsx
@@ -0,0 +1,22 @@
+import { lazy } from 'react';
+
+import { Route, Switch } from 'src/routes/SuspenseRoute';
+const SearchScreen: Lazy = lazy(
+  (): LazyPromise => import(/* webpackPrefetch: true */ '../screens/Product/SearchScreen')
+);
+
+export default function SearchRoutes() {
+  return (
+    <Switch>
+      <Route path="/search/name/:name?" component={SearchScreen} exact />
+      <Route path="/search/category/:category" component={SearchScreen} exact />
+      <Route path="/search/category/:category/order/:order" component={SearchScreen} exact />
+      <Route path="/search/category/:category/name/:name" component={SearchScreen} exact />
+      <Route
+        path="/search/category/:category/name/:name/min/:min/max/:max/rating/:rating/order/:order/pageNumber/:pageNumber"
+        component={SearchScreen}
+        exact
+      />
+    </Switch>
+  );
+}
