ad1fc33e1f1fc93e3d527681debf516bf38d702f
ntrix <ntrix@NTien.com>
2021-09-19 02:06:20 +0200

    ts correct Reducers fn f. better return types

--- apps/amazin/src/slice/UserSlice.ts
+++ apps/amazin/src/slice/UserSlice.ts
@@ -33,10 +33,12 @@ export const { actions: userTopSellerListActions, reducer: userTopSellerListRedu
   adapter('userTopSellerList', { loading: true }, createReducers('users'))
 );
 
+const createMapReducer: FnType = () => ({
+  _CONFIRM: (state: AppState, action: { payload: { type: string; address: AddressType } }) => ({
+    address: action.payload
+  })
+});
+
 export const { actions: userAddressMapActions, reducer: userAddressMapReducer } = createSlice(
-  adapter('userAddressMap', {}, () => ({
-    _CONFIRM: (state: AppState, action: { payload: { type: string; address: AddressType } }) => ({
-      address: action.payload
-    })
-  }))
+  adapter('userAddressMap', {}, createMapReducer())
 );
