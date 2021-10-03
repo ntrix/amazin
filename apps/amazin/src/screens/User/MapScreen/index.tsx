5e0bc7ec392dfaa295a8d37c8f3f3ba668fea56e
ntrix <ntrix@NTien.com>
2021-10-03 23:37:00 +0200

    ts & refact MapScreen setStatus instead setInfo & setError

--- apps/amazin/src/screens/User/MapScreen/index.tsx
+++ apps/amazin/src/screens/User/MapScreen/index.tsx
@@ -4,30 +4,29 @@ import { GoogleMap, LoadScript, Marker, StandaloneSearchBox } from '@react-googl
 import { LOCATION } from 'src/constants';
 import { useMapApiKey, useMap, useMapLocator, useMapSubmit } from './useMapAPIs';
 import MapSearchBox from './MapSearchBox';
-import MessageBox from 'src/components/MessageBox';
 import LoadingBox from 'src/components/LoadingBox';
 import { useSafeState } from 'src/hooks/useSafeState';
+import LoadingOrError from 'src/components/LoadingOrError';
 const libs = ['places'] as ('places' | 'drawing' | 'geometry' | 'localContext' | 'visualization')[];
 
 export default function MapScreen({ history }: RouteProps<MatchParams>) {
   const [center, setCenter] = useSafeState(LOCATION);
   const [location, setLocation] = useSafeState(center);
-  const [error, setError] = useSafeState('');
-  const [info, setInfo] = useSafeState('');
+  const [status, setStatus] = useSafeState({});
   const placeRef = useRef<LocationType>(null);
 
-  const mapAPI = useMapLocator(placeRef, setCenter, setLocation, setError);
+  const mapAPI = useMapLocator(placeRef, setCenter, setLocation, setStatus);
   const { onLoadPlaces, onPlacesChanged, getUserCurrentLocation } = mapAPI;
   const { onLoad, onIdle, onMarkerLoad } = useMap(setLocation);
-  const { onConfirm, redirectBack } = useMapSubmit(history, setInfo, setError);
+  const { onConfirm, redirectBack } = useMapSubmit(history, setStatus);
 
-  const { googleApiKey } = useMapApiKey(getUserCurrentLocation, setError);
+  const { googleApiKey } = useMapApiKey(getUserCurrentLocation, setStatus);
   if (!googleApiKey) return <LoadingBox xl />;
 
   const setting = { zoom: 15, center, onLoad, onIdle };
   return (
     <div className="container--fluid">
-      <MessageBox variant={info ? 'success' : undefined} msg={info || error} />
+      <LoadingOrError statusOf={status} />
 
       <LoadScript libraries={libs} googleMapsApiKey={googleApiKey}>
         <GoogleMap id="sample-map" mapContainerStyle={{ height: '100%', width: '100%' }} {...setting}>
