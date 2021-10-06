63dd5b6b8afa1d57d5a07ebdbf2e8c6020381556
ntrix <ntrix@NTien.com>
2021-10-06 03:28:31 +0200

    refact useKeyInput, reduce cogn. complex

--- apps/amazin/src/components/Nav/NavSearch/SearchBox/SearchInput/useKeyInput.ts
+++ apps/amazin/src/components/Nav/NavSearch/SearchBox/SearchInput/useKeyInput.ts
@@ -3,32 +3,13 @@ import { useShadow } from 'src/hooks/useShadow';
 import { findSuggest, getPlainText } from 'src/utils';
 import { SEARCH, SHADOW } from 'src/constants';
 
-const LAST_S = SEARCH.MAX_SUGGESTS - 1;
-export function useKeyInput(setInput: SetStateType<string>, submitSearch: FnType) {
-  const { productList, suggests, setSuggests, activeSuggest, setActiveSuggest, setOutline, setSuggestBox } =
-    useOutline();
+const LAST_SUGGEST = SEARCH.MAX_SUGGESTS - 1;
+
+export function useInput(setInput: SetStateType<string>, submitSearch: FnType) {
+  const { productList, suggests, setSuggests, activeSuggest, setActiveSuggest, setOutline } = useOutline();
   const { setShadowOf } = useShadow();
 
-  const handleKeyInput: FnType = ({ target: { value }, key }: EventType) => {
-    if (!value) return;
-    setSuggestBox(true);
-
-    switch (key) {
-      case 'ArrowDown':
-        return suggests.length && setActiveSuggest(Math.min(activeSuggest + 1, LAST_S, suggests.length - 1));
-
-      case 'ArrowUp':
-        return suggests.length && setActiveSuggest(Math.max(activeSuggest - 1, 0));
-
-      case 'Enter':
-        if (activeSuggest < 0) return submitSearch();
-        return setInput(getPlainText(suggests?.[activeSuggest]?.name.slice(0, 50)));
-
-      case 'Escape':
-        setActiveSuggest(-1);
-        setSuggestBox(false);
-        return setShadowOf('');
-    }
+  const handleCharInput: FnType = (value: string) => {
     const newSuggests = findSuggest(productList, value);
     setSuggests(newSuggests);
     setActiveSuggest(Math.min(activeSuggest, newSuggests.length - 1));
@@ -37,6 +18,45 @@ export function useKeyInput(setInput: SetStateType<string>, submitSearch: FnType
 
     setShadowOf(SHADOW.NAV_SEARCH);
     return setOutline(true);
+  };
+
+  const handleEnterKey: FnType = (e) => {
+    if (activeSuggest < 0) submitSearch();
+    else setInput(getPlainText(suggests?.[activeSuggest]?.name.slice(0, 50)));
+    e.stopPropagation();
+    setActiveSuggest(-1);
+  };
+
+  return { handleCharInput, handleEnterKey };
+}
+
+export function useKeyInput(setInput: SetStateType<string>, submitSearch: FnType) {
+  const { suggests, activeSuggest, setActiveSuggest, setSuggestBox } = useOutline();
+  const { handleCharInput, handleEnterKey } = useInput(setInput, submitSearch);
+  const { setShadowOf } = useShadow();
+
+  const handleKeyInput: FnType = (e: EventType) => {
+    if (!e.target.value) return;
+    setSuggestBox(true);
+
+    switch (e.key) {
+      case 'ArrowDown':
+        return suggests.length && setActiveSuggest(Math.min(activeSuggest + 1, LAST_SUGGEST, suggests.length - 1));
+
+      case 'ArrowUp':
+        return suggests.length && setActiveSuggest(Math.max(activeSuggest - 1, 0));
+
+      case 'Enter':
+        return handleEnterKey(e);
+
+      case 'Escape':
+        setSuggestBox(false);
+        setActiveSuggest(-1);
+        return setShadowOf('');
+
+      default:
+        handleCharInput(e.target.value);
+    }
   };
 
   return { handleKeyInput };
