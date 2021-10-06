f2a0722369331fc275d49d7041b6817f8860d9bb
ntrix <ntrix@NTien.com>
2021-10-06 13:18:43 +0200

    try redux perf. with rtk immer's advantage

--- apps/amazin/src/slice/ReduxToolKitClient.ts
+++ apps/amazin/src/slice/ReduxToolKitClient.ts
@@ -5,21 +5,14 @@ export { createSlice } from '@reduxjs/toolkit';
 export const createReducers: (
   stateKeyName?: string,
   overwriteReducers?: SliceCaseReducers<unknown>
-) => SliceCaseReducers<unknown> = (stateKeyName = '', overwriteReducers) => ({
+) => SliceCaseReducers<unknown> = (stateKeyName, overwriteReducers) => ({
   _REQUEST: () => ({ loading: true }),
-  _SUCCESS: (_: AppState, action: ActionRedux) => {
+  _SUCCESS: (state: AppState, action: ActionRedux) => {
     // noname saved state?
-    return stateKeyName === '...'
-      ? {
-          ...action.payload, // Array? => destructuring e.g. { a:1, b:2, c:3, loading, success }
-          loading: false,
-          success: true
-        }
-      : {
-          [stateKeyName]: action.payload, // e.g. { state[a]:1, loading, success }
-          loading: false,
-          success: true
-        };
+    if (stateKeyName === '...') state = Object.assign(state, action.payload); // Array? => destructuring e.g. { a:1, b:2, c:3, loading, success }
+    if (stateKeyName) state[stateKeyName] = action.payload; // e.g. { state[a]:1, loading, success }
+    state.loading = false;
+    state.success = true;
   },
   _FAIL: (_: AppState, action: ActionRedux) => ({
     loading: false,
