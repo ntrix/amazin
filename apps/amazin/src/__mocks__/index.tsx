a0ffff46c3c583edc99d21c146fc33ed21417fa5
ntrix <ntrix@NTien.com>
2021-10-13 13:57:03 +0200

    prevent potential errors, note f. test cases

--- apps/amazin/src/__mocks__/index.tsx
+++ apps/amazin/src/__mocks__/index.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { Provider } from 'react-redux';
 import mockStore from './store';
 
-export function AppProviders({ children }: { children: Children }) {
+export function AppProviders({ children }) {
   return <Provider store={mockStore}>{children}</Provider>;
 }
