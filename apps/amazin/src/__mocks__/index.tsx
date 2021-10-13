0133534bcec1741de64fcb90a15fdcb9153832b3
ntrix <ntrix@NTien.com>
2021-10-13 13:08:11 +0200

    amend ts type f. props

--- apps/amazin/src/__mocks__/index.tsx
+++ apps/amazin/src/__mocks__/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { Provider } from 'react-redux';
 import mockStore from './store';
 
-export function AppProviders({ children }) {
+export function AppProviders({ children }: { children: Children }) {
   return <Provider store={mockStore}>{children}</Provider>;
 }
